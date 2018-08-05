e6b441d44fbe09458cc79bd5682390715ec6ac4a
ayya M <ant-heaven@outlook.com>
2018-08-05 13:18:31 +0800

    update signalr server url

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,7 @@ import { SignalRModule, SignalRConfiguration } from 'ng2-signalr';
 export function createConfig(): SignalRConfiguration {
   const c = new SignalRConfiguration();
   c.hubName = 'getwayData';
-  c.url = 'http://221.226.80.114:23333/signalr/hubs';
+  c.url = 'https://ms-mayya.com/signalr/hubs';
   c.logging = false;
   return c;
 }
